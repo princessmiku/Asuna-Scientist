11f9ef058556055e4fa2240e9161bc4fbb1165a1
Miku <mia.nakano@icloud.com>
2022-08-10 12:49:50 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='S2-Asuna',
+    version='',
+    packages=['scientist', 'scientist.search', 'scientist.datahandler'],
+    url='',
+    license='',
+    author='Miku',
+    author_email='',
+    description=''
+)
